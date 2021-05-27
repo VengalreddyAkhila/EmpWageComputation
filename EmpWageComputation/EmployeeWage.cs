7a9cc5d08ba037a414a6619ee80c179c03640d81
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-05-27 13:30:01 +0530

    Refactor:UC5-WagesForMonth

--- EmpWageComputation/EmployeeWage.cs
+++ EmpWageComputation/EmployeeWage.cs
@@ -5,50 +5,56 @@ using System.Text;
 namespace EmpWageComputation
 {
     public class EmployeeWage
-    { 
-            
-            const int IS_PRESENT = 1;
-             const int IS_FULL_TIME = 1;
-             const int IS_PART_TIME = 2;
-             const int EMP_RATE_PER_HOUR = 20;
-             int EmpHrs = 0;
-             int Wage = 0;   
-            public void Attendance()
-            {
-                Random random = new Random();
-                int empCheck = random.Next(0, 2);
-                if (empCheck == IS_PRESENT)
-                    Console.WriteLine("Employee is present");
-                else
-                    Console.WriteLine("Employee is absent");
-            }
+    {
+
+        const int IS_PRESENT = 1;
+        const int IS_FULL_TIME = 1;
+        const int IS_PART_TIME = 2;
+        const int EMP_RATE_PER_HOUR = 20;
+        const int NUMBER_OF_WORKING_DAYS = 20;
+        int EmpHrs = 0;
+        int Wage = 0;
+        int totalempwage = 0;
+        public void Attendance()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_PRESENT)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+        }
         public void Calculatewage()
         {
             int EmpHrs = 8;
             int Wage = EMP_RATE_PER_HOUR * EmpHrs;
             Console.WriteLine("wage" + Wage);
         }
-        public void PartTime()
+        public void WagesForMonth()
         {
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            for (int day = 0; day < NUMBER_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    EmpHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    EmpHrs = 4;
-                    break;
-                default:
-                    EmpHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        EmpHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        EmpHrs = 4;
+                        break;
+                    default:
+                        EmpHrs = 0;
+                        break;
+                }
+                Wage = EmpHrs * EMP_RATE_PER_HOUR;
+                totalempwage = totalempwage + Wage;
+                Console.WriteLine("wage:" + Wage);
             }
-            Wage = EmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("wage:" + Wage);
+            Console.WriteLine(" totalempwage: " + totalempwage);
         }
-
-
     }
-    
 }
+    
+
--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -7,8 +7,8 @@ namespace EmpWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Emp Wage Computation");
-            EmpWage empwage = new EmpWage();
-            empwage.PartTime();
+            EmployeeWage employeewage = new EmployeeWage();
+            employeewage.WagesForMonth();
         }
     }
 }
